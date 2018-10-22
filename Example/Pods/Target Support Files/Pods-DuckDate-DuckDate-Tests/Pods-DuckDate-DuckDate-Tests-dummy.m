cc853c717f07235a5e0560154d97576373997386
Yura Voevodin <voevodin.yura@icloud.com>
2018-10-22 11:51:24 +0300

    Fixed issue with macOS version of library.

--- Example/Pods/Target Support Files/Pods-DuckDate-DuckDate-Tests/Pods-DuckDate-DuckDate-Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DuckDate_DuckDate_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_DuckDate_DuckDate_Tests
-@end
--- Example/Pods/Target Support Files/Pods-DuckDate-iOS/Pods-DuckDate-iOS-umbrella.h
+++ Example/Pods/Target Support Files/Pods-DuckDate-iOS/Pods-DuckDate-iOS-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_DuckDate_DuckDate_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_DuckDate_DuckDate_TestsVersionString[];
+FOUNDATION_EXPORT double Pods_DuckDate_iOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DuckDate_iOSVersionString[];
 
